0ebe06b0d7cc8c7836e207b29040e662225cc8f2
sooka-tv <104600997+sooka-tv@users.noreply.github.com>
2023-04-03 10:09:07 +0800

    Update results.php

--- api/results.php
+++ api/results.php
@@ -8,7 +8,7 @@ $hex = $_GET["keyid"];            // and much more hex values as string as in yo
 $bin = hex2bin($hex);       // convert the hex values to binary data stored as a PHP string
 $keyid64 = base64_encode($bin); // remove == , replace as space
 $finalkeyid64 = str_replace('=', '', $keyid64);
-
+$finalkeyid64 = str_replace('\', '', $keyid64);
 
 // validation
 if (empty($finalkeyid64)){
@@ -28,7 +28,7 @@ $hex2 = $_GET["key"];            // and much more hex values as string as in you
 $bin2 = hex2bin($hex2);       // convert the hex values to binary data stored as a PHP string
 $key64 = base64_encode($bin2); // remove == , replace as space
 $finalkey64 = str_replace('=', '', $key64);
-
+$finalkey64 = str_replace('\', '', $key64);
 // validation
 
 if (empty($finalkey64)){
